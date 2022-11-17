7a6e136e31c194e21da3f3449c824a052cb01862
alwiramadhan <alwiramadhanhhrp@gmail.com>
2022-11-18 00:22:33 +0700

    firs commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 2,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut odio facere suscipit?",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut odio facere suscipit?",
+  },
+  {
+    id: 3,
+    rating: 6,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut odio facere suscipit?",
+  },
+];
+
+export default FeedbackData;
